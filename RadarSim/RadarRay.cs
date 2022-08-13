9ad43a8dac594ac108d6a9abc31009cd8d1e9a65
Морозов Юрий <moroz69off@gmail.com>
2022-08-14 00:01:52 +0300

    ray position

--- RadarSim/RadarRay.cs
+++ RadarSim/RadarRay.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Drawing;
 
 namespace RadarSim
 {
 	internal class RadarRay
 	{
+		public PointF RayCenter { get; } = new PointF();
+		public PointF RayRadius { get; } = new PointF();
+
 		public RadarRay()
 		{
+
 		}
 
 		internal void Rotate(double v)
