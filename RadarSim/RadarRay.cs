6182e7f0d8f186ac2dc5f302a9c6b49f233c834e
Морозов Юрий <moroz69off@gmail.com>
2022-12-19 12:30:58 +0300

    add ray
    Signed-off-by: Морозов Юрий <moroz69off@gmail.com>

---
+++ RadarSim/RadarRay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace RadarSim
+{
+	internal class RadarRay
+	{
+		public PointF RayCenter { get; }
+		public PointF RayEnd { get; set; }
+
+		public RadarRay()
+		{
+
+		}
+
+		internal void Rotate(double v)
+		{
+			Console.WriteLine(v);
+		}
+	}
+}
